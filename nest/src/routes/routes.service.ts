bff1e5f47c2b9cd9e86b53d7acec5d8a6e078e28
Caio Maciel <caiolmaciell@gmail.com>
2023-10-29 17:41:02 -0300

    :bug: fix: Code smells e hotfix

--- nest/src/routes/routes.service.ts
+++ nest/src/routes/routes.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
-// import { UpdateRouteDto } from './dto/update-route.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { DirectionsService } from 'src/maps/directions/directions.service';
 
